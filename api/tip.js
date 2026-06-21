dce10cbfa55e0078f8d6ea3c775851aa46a35e9e
Jasmine <lumina.jasmine98@gmail.com>
2026-06-20 21:27:47 -0300

    debug: expose full Stripe error details

--- api/tip.js
+++ api/tip.js
@@ -28,6 +28,6 @@ export default async function handler(req, res) {
     })
     return res.status(200).json({ url: session.url })
   } catch (err) {
-    return res.status(500).json({ error: err.message })
+    return res.status(500).json({ error: err.message, type: err.type, code: err.code, name: err.name })
   }
 }
